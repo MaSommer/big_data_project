2d495c5316b43103b730827117bb0a9a8a6ee1f3
MaSommer <martisom@stud.ntnu.no>
2017-03-03 16:22:51 +0100

    cunt

---
+++ Line_Count_Spark/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SparkConf conf = new SparkConf().setAppName("TheLegends");
+		
+		JavaSparkContext ctx = new JavaSparkContext(conf);
+		
+		JavaRDD<String> textLoadRDD = ctx.textFile("/Users/MaSommer/Google Drive/Skole/4.Year/Vaar/BigData/HADOOP_HOME.3/spark-2.1.0-bin-hadoop2.7/README.md");
+		
+		System.out.println(textLoadRDD.count());
+		
+	}
+
+}
